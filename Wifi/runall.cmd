803abd60b809ff948deb02b341853ab9970ed258
Bas Magré <Opvolger@gmail.com>
2019-02-12 21:44:21 +0100

    niet verbergen

--- Wifi/runall.cmd
+++ Wifi/runall.cmd
@@ -1,3 +1,3 @@
-powershell -WindowStyle Hidden -executionpolicy bypass -File .\GetWifiCreds.ps1 -ErrorAction SilentlyContinue > output.txt
+powershell -executionpolicy bypass -File .\GetWifiCreds.ps1 -ErrorAction SilentlyContinue > output.txt
 call netsh wlan show wlanreport
 type C:\ProgramData\Microsoft\Windows\WlanReport\wlan-report-latest.html > wlan-report-latest.html
